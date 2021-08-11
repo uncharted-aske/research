e092b5f1794b2ca35841bae485dc882a75cb46f0
Nelson Liu <nliu@uncharted.software>
2021-08-11 15:57:39 +0000

    UAZ's new outputs

---
+++ gromet/data/uaz/loop/loop_ex2.py
@@ -0,0 +1,7 @@
+def loop_ex2(k: int):
+    e = 5
+    for i in range(e):  # loop_1
+        # i >= 5        # loop_1_cond
+        # i = i + 1     # loop_1_i_exp
+        k = k + 1       # loop_1_k_exp
+    return k
