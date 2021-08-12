ffcbf846406efcfb6d92df8d10e3d5d652205a8b
Nelson Liu <nliu@uncharted.software>
2021-08-12 21:35:38 +0000

    Tested parser on loop, conditional, chime gromets

--- gromet/notebooks/8_uaz_gromet_testing.py
+++ gromet/notebooks/8_uaz_gromet_testing.py
@@ -9,6 +9,7 @@
 # * 
 
 # %%
+import os
 import json
 import networkx as nx
 import numpy as np
@@ -16,80 +17,32 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 from typing import Dict, Tuple, Any, Optional
 
-# %%
-# # Load the Loop GroMEt
+# %%[markdown]
+# # Run the Dario parser over the GroMEt
 
-uaz_path = "/home/nliu/projects/aske/research/gromet/data/uaz/"
+deno_command = 'deno run --allow-write --allow-read'
+parser_path = '/home/nliu/projects/aske/research/gromet/tools/parse.ts'
+data_dir = '/home/nliu/projects/aske/research/gromet/data/uaz/'
+dist_dir = '/home/nliu/projects/aske/research/gromet/dist/uaz/'
 
-with open(uaz_path + 'loop/loop_ex2_gromet_FunctionNetwork.json', 'r') as f:
-    gromet_loop = json.load(f)
+gromet = []
+graph = []
+for p in ('loop/loop_ex2_gromet_FunctionNetwork_correction', 'conditional/cond_ex1_gromet_FunctionNetwork', 'CHIME/CHIME_SIR_v01_gromet_FunctionNetwork_by_hand'):
 
-with open(uaz_path + 'conditional/cond_ex1_gromet_FunctionNetwork.json', 'r') as f:
-    gromet_cond = json.load(f)
+    gromet_path = data_dir + f'{p}.json'
+    graph_path = dist_dir + f'{p}_graph.json'
 
-f = None
-del f
+    __ = os.system(deno_command + ' ' + parser_path + ' ' + gromet_path + ' ' + graph_path)
 
-# %%
-# Box-type nodes
-nodes = {node['uid']: {
-    'id': node['uid'],
-    'concept': node['syntax'],
-    'role': [],
-    'label': node['uid'],
-    'nodeType': 'Box',
-    'dataType': None if 'value_type' not in node.keys() else node['value_type'],
-    'parent': None,
-    # 'children': [],
-    'nodeSubType': [node['type']],
-    'metadata': node['metadata']
-} for node in gromet_loop['boxes']}
+    with open(gromet_path, 'r') as f:
+        gromet.append(json.load(f))
 
-# Other node types
-for k in ('ports', 'junctions'):
-
-    for node in gromet_loop[k]:
-
-        nodes[node['uid']] = {
-            'id': node['uid'],
-            'concept': node['syntax'],
-            'role': [],
-            'label': node['uid'],
-            'nodeType': node['syntax'],
-            'dataType': None if 'value_type' not in node.keys() else node['value_type'],
-            'parent': None,
-            # 'children': [],
-            'nodeSubType': [node['type']],
-            'metadata': node['metadata']
-        }
-
-# %%
-# Parenthood
-for node in gromet_loop['boxes']:
-    for k in ('ports', 'junctions', 'exit_condition'):
-        if k in node.keys():
-            if node[k] != None:
-                if isinstance(node[k], list):
-                    for m in node[k]:
-                        nodes[m]['parent'] = node['uid']
-                else:
-                    nodes[m]['parent'] = node['uid']
-
-# Role
-for k in ('variables', 'parameters', 'initial_conditions'):
-    for m in gromet_loop['metadata'][0][k]:
-        nodes[m]['role'] += k
+    with open(graph_path, 'r') as f:
+        graph.append(json.load(f))
 
 
-# %%
-# Edges
-edges = [{
-    'source': wire['src'],
-    'target': wire['tgt']
-} for wire in gromet_loop['wires']]
-
-
-# edges += {'source': 'P:loop_1_in.i', 'target': 'B:loop_1'}
+deno_command = parser_path = data_dir = dist_dir = gromet_path = f = p = None
+del deno_command, parser_path, data_dir, dist_dir, gromet_path, f, p
 
 # %%
 
@@ -107,11 +60,46 @@ edges = [{
 # gromet_loop['wires'][1]['src'] = 'P:loop_1.in.e'
 
 # %%
-# Parsed graph
-graph_loop = {'nodes': [node for __, node in nodes.items()], 'edges': edges, 'metadata': gromet_loop['metadata']}
+# Calculate hierarchy level of a node from its ID (e.g. "0::12:11" = 0 -> 12 -> 11)
+def calculate_node_level(node_id: str) -> int:
 
+    node_level = len(node_id.split('::')) - 1
 
-# %%
+    return node_level
+
+# Get children of all nodes in graph
+def get_node_children(graph: Dict) -> Dict:
+
+    node_children = {node['id']: [] for node in graph['nodes']}
+    for node in graph['nodes']:
+        if node['parent'] != None:
+            node_children[node['parent']].append(node['id'])
+
+    return node_children
+
+# Get children of all nodes in NX graph object
+def get_node_children_nx(G: Dict) -> Dict:
+
+    node_children = {node['id']: [] for node in G.nodes}
+    for node in G.nodes:
+        if G.nodes[node]['parent'] != None:
+            node_children[G.nodes[node]['parent']].append(node)
+
+    return node_children
+
+# Get all descendants of every node in NX graph object
+def get_node_descendants_nx(G: Dict) -> Dict:
+
+    node_descendants = {node: [] for node in G.nodes}
+    for node in G.nodes:
+        k = node.split('::')[:-1]
+        for i in range(1, len(k) + 1):
+            node_parent = '::'.join(k[0:i])
+            if node_parent in node_descendants.keys():
+                node_descendants[node_parent].append(node)
+
+    return node_descendants
+
 # Generate NX Object from a parsed GroMEt
 def generate_nx_obj(graph: Dict) -> Any:
 
@@ -131,19 +119,69 @@ def generate_nx_obj(graph: Dict) -> Any:
 
     return G
 
-# Get all descendants of every node in NX graph object
-def get_node_descendants_nx(G: Dict) -> Dict:
+# Add equi-level edges
+# If an edge exists between a level-k node and a level-(k + 1) one, 
+# add an edge between the level-k node and the level-k parent of the other
+def add_missing_edges(G: Any) -> Any:
 
-    node_descendants = {node: [] for node in G.nodes}
-    for node in G.nodes:
-        k = node.split('::')[:-1]
-        for i in range(1, len(k) + 1):
-            node_parent = '::'.join(k[0:i])
-            node_descendants[node_parent].append(node)
+    edges = []
+    for edge in G.edges:
 
-    return node_descendants
+        m = calculate_node_level(edge[0])
+        n = calculate_node_level(edge[1])
+        
+        if m != n:
+            
+            i = np.argmin([m, n])
+            if i == 0:
+                src = G.nodes[edge[0]]['id']
+                tgt = G.nodes[edge[1]]['parent']
+            else:
+                src = G.nodes[edge[0]]['parent']
+                tgt = G.nodes[edge[1]]['id']
 
-# %%
+            if src != tgt:
+                edges.append((src, tgt, 0))
+
+
+    G.add_edges_from(edges, weight = 0)
+
+    return None
+
+# Promote edges between nodes of different parent nodes to edges between the parent nodes
+# e.g. "0::1::2"->"0::2::3" becomes "0::1"->"0::2"
+# "0::3"->"0:28:27" becomes "0::3"->"0:28"
+def promote_edges(G: Any) -> Any:
+
+    edges_remove = []
+    edges_add = []
+
+    for edge in G.edges:
+        src_parent = G.nodes[edge[0]]['parent']
+        tgt_parent = G.nodes[edge[1]]['parent']
+
+        if src_parent != tgt_parent:
+
+            m = calculate_node_level(edge[0])
+            n = calculate_node_level(edge[1])
+            
+            if m != n:
+                k = min([m, n]) + 1
+                src = '::'.join(edge[0].split('::')[:k])
+                tgt = '::'.join(edge[1].split('::')[:k])
+                edges_add.append((src, tgt))
+
+            else:
+                edges_add.append((src_parent, tgt_parent))
+
+            edges_remove.append(edge)
+
+
+    G.remove_edges_from(edges_remove)
+    G.add_edges_from(edges_add, weight = -1)
+
+    return None
+
 # Generate linear layout from a NetworkX graph using condensation & topological sorting
 def generate_linear_layout(G: Any, offset: Optional[Dict] = None, draw: Optional[bool] = False, ax: Optional[Any] = None) -> Any:
 
@@ -171,44 +209,6 @@ def generate_linear_layout(G: Any, offset: Optional[Dict] = None, draw: Optional
 
     return pos
 
-# %%
-# Generate linear layout that is layered by the parent-child hierarchy of the graph
-def generate_linear_layout_with_hierarchy(G: Any, draw: Optional[bool] = False, ax: Optional[Any] = None) -> Dict:
-
-    if (draw == True) & (ax == None):
-        fig, ax = plt.subplots(nrows = 1, ncols = 1, figsize = (20, 5))
-
-    pos_full = {None: np.array([0, -1])}
-    max_node_level = max([calculate_node_level(node) for node in G.nodes])
-
-    node_descendants = get_node_descendants_nx(G)
-    num_descendants = {node: len(l) for node, l in node_descendants.items()}
-
-
-    for l in range(max_node_level + 1):
-
-        nodes = [node for node in G.nodes if calculate_node_level(node) == l]
-        parents = set([G.nodes[node]['parent'] for node in nodes])
-
-        for parent in parents:
-
-            nodes_ = [node for node in G.nodes if G.nodes[node]['parent'] == parent]
-            G_sub = G.subgraph(nodes_)
-            pos = generate_linear_layout(G = G_sub, offset = num_descendants)
-            pos = {k: v + pos_full[parent] + np.array([0, 1]) for k, v in pos.items()}
-            pos_full = {**pos_full, **pos}
-
-            if draw == True:
-                __ = draw_graph(G_sub, pos = pos, ax = ax, G_full = G)
-
-    if draw == True:
-        __ = plt.setp(ax, ylim = (-1, max_node_level + 1))
-    
-    return pos_full
-
-
-
-# %%
 # Draw NX Object
 def draw_graph(G: Any, pos: Dict, ax: Optional[Any] = None, node_args: Optional[Dict] = None, edge_args: Optional[Dict] = None, label_args: Optional[Dict] = None, legend_args: Optional[Dict] = None, G_full: Optional[Any] = None, label_key: Optional[str] = 'label') -> Any:
 
@@ -284,19 +284,74 @@ def draw_graph(G: Any, pos: Dict, ax: Optional[Any] = None, node_args: Optional[
 
     return None
 
+# Generate linear layout that is layered by the parent-child hierarchy of the graph
+def generate_linear_layout_with_hierarchy(G: Any, draw: Optional[bool] = False, ax: Optional[Any] = None) -> Dict:
+
+    if (draw == True) & (ax == None):
+        fig, ax = plt.subplots(nrows = 1, ncols = 1, figsize = (20, 5))
+
+    pos_full = {None: np.array([0, -1])}
+    max_node_level = max([calculate_node_level(node) for node in G.nodes])
+
+    node_descendants = get_node_descendants_nx(G)
+    num_descendants = {node: len(l) for node, l in node_descendants.items()}
+
+
+    for l in range(max_node_level + 1):
+
+        nodes = [node for node in G.nodes if calculate_node_level(node) == l]
+        parents = set([G.nodes[node]['parent'] for node in nodes])
+
+        for parent in parents:
+
+            nodes_ = [node for node in G.nodes if G.nodes[node]['parent'] == parent]
+            G_sub = G.subgraph(nodes_)
+            pos = generate_linear_layout(G = G_sub, offset = num_descendants)
+            pos = {k: v + pos_full[parent] + np.array([0, 1]) for k, v in pos.items()}
+            pos_full = {**pos_full, **pos}
+
+            if draw == True:
+                __ = draw_graph(G_sub, pos = pos, ax = ax, G_full = G)
+
+    if draw == True:
+        __ = plt.setp(ax, ylim = (-1, max_node_level + 1))
+    
+    return pos_full
+
+
 # %%
 
+G = generate_nx_obj(graph = graph[0])
 
-G = generate_nx_obj(graph = graph_loop)
-# __ = add_missing_edges(G = G)
-# __ = promote_edges(G = G)
-__ = generate_linear_layout(G, draw = True)
-# __ = generate_linear_layout_with_hierarchy(G, draw = True)
+G.remove_edge('0::6::14', '0::6::5')
+G.remove_edge('0::6::15', '0::6::13')
+G.remove_edge('0::6::10', '0::6::8')
 
+__ = add_missing_edges(G = G)
+__ = promote_edges(G = G)
+# __ = generate_linear_layout(G, draw = True)
+
+__ = generate_linear_layout_with_hierarchy(G, draw = True)
+
+# %%
+
+G = generate_nx_obj(graph = graph[1])
+
+__ = add_missing_edges(G = G)
+__ = promote_edges(G = G)
+# __ = generate_linear_layout(G, draw = True)
+
+__ = generate_linear_layout_with_hierarchy(G, draw = True)
 
 
 # %%
-G_loop = generate_nx_obj(gromet = gromet_loop)
+G = generate_nx_obj(graph = graph[2])
 
-G_map, fig = compare_graphs(G1 = G_loop, G2 = G_loop, map = None, leg_id = None, rename = ('Loop1 ', 'Loop2 '), plot = True, plot_layout = 'linear')
-# fig.savefig(f'../figures/map_sir_chime+_{i}.png', dpi = 150)
+__ = add_missing_edges(G = G)
+__ = promote_edges(G = G)
+# __ = generate_linear_layout(G, draw = True)
+
+__ = generate_linear_layout_with_hierarchy(G, draw = True)
+
+
+# %%
